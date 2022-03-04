ed9be7ce07c882d6646876ce79bcebf1df546fe4
Gita Rahmi <gitarahmifajrin01@gmail.com>
2022-03-04 17:56:51 +0700

    tambahan fitur GUI

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -64,7 +64,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 252);
+            this.ClientSize = new System.Drawing.Size(457, 291);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
